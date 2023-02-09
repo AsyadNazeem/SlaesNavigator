1820b2ba0e8a4f6b1e660761dc39f3537ea28a9e
MacbookAir <asyadnazeem@gmail.com>
2023-02-09 12:57:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItem = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItem += `
+        <li>
+            <a target="_blank" href="${leads[i]}">
+                ${leads[i]}
+            </a> 
+        </li>`
+    }
+    ulEL.innerHTML = listItem
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+ 
+
+
